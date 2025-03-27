79ecfb11885cbb5aeb1ac969beff4392402d5c34
Khakimov-292 <kyubi292@yandex.ru>
2025-03-27 22:03:45 +0300

    Delete Sprint_5 directory

--- Sprint_5/data.py
+++
@@ -1,4 +0,0 @@
-surname = 'khakimov'
-number_kogort = 19
-name = 'timur'
-domain = 'yandex.ru'
--- Sprint_5/urls.py
+++
@@ -1,3 +0,0 @@
-BaseURL = "https://stellarburgers.nomoreparties.site/"
-ProfileURL = "https://stellarburgers.nomoreparties.site/account/profile"
-LoginURL = "https://stellarburgers.nomoreparties.site/login"
